a7aebc01b1b8a34d4f99e701352cbc9a1441503e
duraikutty <c.duraikutty@gmail.com>
2021-06-18 22:20:01 +0530

    Angular flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
  
+  mediaSub: Subscription;
+  deviceXs: boolean;
+  constructor(public mediaObserver: MediaObserver) {
+    this.deviceXs = false;
+    this.mediaSub = Subscription.EMPTY;
+  }
+  ngOnInit() {
+    this.mediaSub = this.mediaObserver.media$.subscribe((res: MediaChange) => {
+      console.log(res.mqAlias);
+      this.deviceXs = res.mqAlias === "xs" ? true : false;
+    })
+  }
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = 'Bethel Industries';
+  @Input()
+  deviceXs : boolean;
   
+  title = 'Bethel Industries';
+
   constructor() { }
 
   ngOnInit(): void {
